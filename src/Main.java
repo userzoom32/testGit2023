1731c40c8e75958590dda12057f7bb707340dba1
userzoom32 <itclasszoom32@mail.ru>
2024-08-20 13:10:07 +0300

    123

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("1111");
         System.out.println("22222222");
+        System.out.println("22222222");
     }
 }
